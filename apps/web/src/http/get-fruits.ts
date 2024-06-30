c280e9361122acd6c56e2c50f9f33b3b175ead86
Bilal Meddah <bilal.meddah@epfl.ch>
2024-06-30 16:06:58 +0100

    feat(web): add purchase page

--- apps/web/src/http/get-fruits.ts
+++ apps/web/src/http/get-fruits.ts
@@ -1,6 +1,6 @@
 import rpc from "./rpc";
 
-export async function retrieveAvailableFruits() {
+export default async function retrieveAvailableFruits() {
     const response = await rpc.api.common.fruits.$get();
     if (response.ok) {
         const result = await response.json();
